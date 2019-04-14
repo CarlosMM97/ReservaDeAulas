6480e5a42f558556c81cfabbb79f948ea53fa19a
Carlos Martin Machado Mechetti <martincm.cmmm@gmail.com>
2019-04-14 20:35:27 +0200

    Comit 2

--- ReservaDeAulas/Profesor.cs
+++ ReservaDeAulas/Profesor.cs
@@ -8,14 +8,18 @@ namespace ReservaDeAulas
 {
     public class Profesor
     {
-        private int Profesor_ID;
-        public string NombreApellido;
-        public string NroDocumento;
-        public string FechaNacimiento;
-        public string Mail;
+        private int profesor_ID;
+        private string nombreApellido;
+        private string nroDocumento;
+        private string nechaNacimiento;
+        private string nail;
+        private int tipoDocumento_tipoDocumntoID;
 
-
-
-
+        public int Profesor_ID { get => profesor_ID; set => profesor_ID = value; }
+        public string NombreApellido { get => nombreApellido; set => nombreApellido = value; }
+        public string NroDocumento { get => nroDocumento; set => nroDocumento = value; }
+        public string NechaNacimiento { get => nechaNacimiento; set => nechaNacimiento = value; }
+        public string Nail { get => nail; set => nail = value; }
+        public int TipoDocumento_tipoDocumntoID { get => tipoDocumento_tipoDocumntoID; set => tipoDocumento_tipoDocumntoID = value; }
     }
 }
