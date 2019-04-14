d55a7a45618f0cedcd875548be625046df0a3590
Carlos Martin Machado Mechetti <martincm.cmmm@gmail.com>
2019-04-14 21:24:37 +0200

    comit 7

---
+++ ReservaDeAulas/Materia.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaDeAulas
+{
+    class Materia
+    {
+        private int materia_ID;
+        private string descripcion;
+
+        public int Materia_ID
+        {
+            get
+            {
+                return materia_ID;
+            }
+
+            set
+            {
+                materia_ID = value;
+            }
+        }
+        public string Descripcion
+        {
+            get
+            {
+                return descripcion;
+            }
+
+            set
+            {
+                descripcion = value;
+            }
+        }
+    }
+}
